7f4b932099ba89a925f143f551957039257ee787
puneetdubey <kkmm44452@gmail.com>
2026-07-20 02:24:52 +0530

    Update API URL for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // apiBaseUrl: 'http://localhost:5007/api'
 
-   apiBaseUrl: 'http://punitaspnetcrud.somee.com/api'
+   apiBaseUrl: 'https://punitaspnetcrud.somee.com/api'
 };
